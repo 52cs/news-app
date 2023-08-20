e8a0a623ab97539e65448dbfb5166c59150aded2
52code <dlmubupt@gmail.com>
2023-08-20 22:23:57 +0800

    finalize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import fetchNews from "@/lib/fetchNews"
 import { categories } from "@/constants"
 import NewsList from "./NewsList"
-import response from '../../response.json'
 
 export default async function Home() {
-  const news: NewsResponse =
-    response as NewsResponse || await fetchNews(categories.join(','))
+  const news: NewsResponse = await fetchNews(categories.join(','))
 
   return (
     <div>
