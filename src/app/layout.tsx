f8ce96313275248a150106080d9d4c87a06a4715
52code <dlmubupt@gmail.com>
2023-08-20 19:38:59 +0800

    News List & dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from './Header'
+import Providers from './Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,12 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-gray-100 dark:bg-zine-900 transition-all duration-700">
-        <Header />
-        <div className="max-w-6xl mx-auto">
-          {children}
-        </div>
-      </body>
+      <Providers>
+        <body className="bg-gray-100 dark:bg-zinc-900 transition-all duration-700">
+          <Header />
+          <div className="max-w-6xl mx-auto">
+            {children}
+          </div>
+        </body>
+      </Providers>
     </html>
   )
 }
